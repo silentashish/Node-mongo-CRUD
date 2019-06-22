bfded89081f2f09104542061106aa7cdc0af639e
gashish <ashish.gm74@gmail.com>
2019-06-22 21:54:26 +0545

    Intitial Work

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Node Js CRUD with mongodb database
+
+This repo contain work in Node js to perform basic work, which are (CRUD)
+- Create
+- Read
+- Update
+- Delete
---
+++ controllers/product.controller.js
@@ -0,0 +1,33 @@
+const Product = require('../models/product.model');
+
+//Simple version, without validation or sanitation
+exports.test = function (req, res) {
+    res.send('Greetings from the Test controller!');
+};
+
+exports.product_create = function (req, res) {
+    console.log(req.body.name +" "+req.body.price);
+    let product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    );
+
+    product.save(function (err) {
+        if (err) {
+            // return next(err);
+            console.log(err);
+        }
+        res.send('Product Created successfully')
+    })
+};
+
+exports.product_details = function (req, res) {
+    Product.findById(req.params.id, function (err, product) {
+        if (err){
+            console.log(err);
+        }
+        res.send(product);
+    })
+};
